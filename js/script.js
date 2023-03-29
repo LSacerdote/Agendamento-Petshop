33bd0970db2b1faa6c0b5bc3c7d1482b66a49985
Lsacerdote <lucassacerdote227@gmail.com>
2023-03-28 21:44:34 -0300

    input checkbox

--- js/script.js
+++ js/script.js
@@ -4,22 +4,24 @@ function gotowhatsapp() {
     var telefone = document.getElementById("telefone").value;
     var service = document.getElementById("service").value;
     var porte = document.getElementById("porte").value;
-    var desembolo = document.getElementById("desembolo").value;
-    var corte = document.getElementById("corte").value;
-    var hidratação = document.getElementById("hidratação").value;
-    var tratamento = document.getElementById("tratamento").value;
-    var tonalização = document.getElementById("tonalização").value;
-    var limpeza = document.getElementById("limpeza").value;
-    var taxidog = document.getElementById("taxidog").value;
+    var endereço = document.getElementById("endereço").value;
+      var opcoes = document.getElementsByName('opcao');
+      var valores_selecionados = '';
+      for (var i = 0; i < opcoes.length; i++) {
+        if (opcoes[i].checked) {
+          valores_selecionados += opcoes[i].value + ', ';
+        }
+      }
     var url = "https://wa.me/5511984379284?text=" 
     
       + "Nome: " + nome + "%0a" 
       + "Nome do Animal: " + animal + "%0a"
       + "Telefone: " + telefone + "%0a"
+      + "Endereço: " + endereço + "%0a"
       + "Serviço: " + service + "%0a"
       + "Porte do Animal: " + porte + "%0a"
-      + "Adicionais: " + desembolo +"%0a"
-      + "Endereço: " + taxidog + "%0a"
+      + "Adicionais: " + valores_selecionados;
+    
 
     window.open(url, '_blank').focus();
 
